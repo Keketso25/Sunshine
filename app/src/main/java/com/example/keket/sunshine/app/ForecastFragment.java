3768afccbca4cfc64eb982c476397959a59694b5
Keketso25 <keketso1@hotmail.com>
2016-11-18 15:54:40 +0200

    BuiltUrl

--- app/src/main/java/com/example/keket/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/keket/sunshine/app/ForecastFragment.java
@@ -2,6 +2,7 @@ package com.example.keket.sunshine.app;
 
 
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -63,7 +64,7 @@ public class ForecastFragment extends Fragment {
         if(id == R.id.action_refresh){
             //To do code here
             FetchWeatherTask fetchWeatherTask = new FetchWeatherTask();
-            fetchWeatherTask.execute();
+            fetchWeatherTask.execute("johannesburg");
             return true;
         }
 
@@ -110,13 +111,24 @@ public class ForecastFragment extends Fragment {
         return rootView;
     }
 
-    public class FetchWeatherTask extends AsyncTask<Void, Void, Void>{
+    public class FetchWeatherTask extends AsyncTask<String, Void, Void>{
 
         //Variable Declaration
         private final String LogTag = FetchWeatherTask.class.getSimpleName();
+        String format = "json";
+        String units = "metric";
+        String appid = "3cfc1fa8af75f1693254776a7dd1071b";
+        int numDays = 7;
 
         @Override
-        protected Void doInBackground(Void... params) {
+        protected Void doInBackground(String... params) {
+
+            // If there's no zip code, there's nothing to look up.  Verify size of params.
+            if (params.length == 0) {
+                return null;
+            }
+
+
 
             // These two need to be declared outside the try/catch
             // so that they can be closed in the finally block.
@@ -130,7 +142,27 @@ public class ForecastFragment extends Fragment {
                 // Construct the URL for the OpenWeatherMap query
                 // Possible parameters are avaiable at OWM's forecast API page, at
                 // http://openweathermap.org/API#forecast
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast?q=johannesburg,za&mode=json&appid=3cfc1fa8af75f1693254776a7dd1071b");
+                //URL url = new URL("http://api.openweathermap.org/data/2.5/forecast?q=johannesburg,za&mode=json&appid=3cfc1fa8af75f1693254776a7dd1071b");
+
+                final String FORECAST_BASE_URL =
+                        "http://api.openweathermap.org/data/2.5/forecast/daily?";
+                final String QUERY_PARAM = "q";
+                final String FORMAT_PARAM = "mode";
+                final String UNITS_PARAM = "units";
+                final String DAYS_PARAM = "cnt";
+                final String KEY_PARAM = "appid";
+
+                Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                        .appendQueryParameter(QUERY_PARAM, params[0])
+                        .appendQueryParameter(FORMAT_PARAM, format)
+                        .appendQueryParameter(UNITS_PARAM, units)
+                        .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                        .appendQueryParameter(KEY_PARAM, appid)
+                        .build();
+
+                URL url = new URL(builtUri.toString());
+
+                Log.v(LogTag, "Built URI " + builtUri.toString());
 
                 // Create the request to OpenWeatherMap, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
